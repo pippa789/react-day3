5200f72a84f06b5e9e673414d090eb7fbf137f4d
unknown <allenpip341@gmail.com>
2023-03-02 15:37:51 +0000

    exercise 4 movie

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Form from "./components/Form";
+import OmdbContainer from "./components/OmdbContainer";
 
 function App() {
-  return <Form />;
+  return <OmdbContainer />;
 }
 
 export default App;
